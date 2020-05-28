3a8f4c7ea37df3acdbb4668fcc8faa6763d978b2
Fatead <893051689@qq.com>
2020-05-28 08:56:20 +0800

    25 K个一组翻转链表

--- src/list/SwapKNode.java
+++ src/list/SwapKNode.java
@@ -10,13 +10,55 @@ public class SwapKNode {
         }
     }
 
-//    public ListNode swapPairs(ListNode head,int k) {
-//
-//    }
-//
-//    public ListNode reverseKGroup(ListNode head, int k) {
-//
-//    }
+
+    private ListNode reverse(ListNode head){
+        ListNode pre = null;
+        ListNode curr = head;
+        while(curr!=null){
+            ListNode next = curr.next;
+            curr.next = pre;
+            pre = curr;
+            curr = next;
+        }
+        return pre;
+    }
+
+    public ListNode reverseKGroup(ListNode head, int k) {
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode end= dummy;
+        ListNode pre = dummy;
+        while(end.next!=null){
+            for(int i =0;i<k&&end!=null;i++){
+                end = end.next;
+            }
+            if(end == null)break;
+            ListNode start = pre.next;
+            ListNode next = end.next;
+            end.next = null;
+            pre.next = reverse(start);
+            start.next = next;
+            pre = start;
+            end = pre;
+        }
+        return dummy.next;
+    }
+
+    public static void main(String[] args) {
+        ListNode head = new ListNode(0);
+        ListNode pt = head;
+        for(int i =1;i<5;i++){
+            ListNode node = new ListNode(i);
+            pt.next = node;
+            pt = node;
+        }
+        SwapKNode swapKNode = new SwapKNode();
+        head =  swapKNode.reverseKGroup(head,2);
+        while(head!=null){
+            System.out.println(head.val);
+            head = head.next;
+        }
+    }
 
 
 }
