0510cf26fdb121e87d65be536221f5543a538a73
Fatead <893051689@qq.com>
2020-06-08 09:03:28 +0800

    打卡32最长有效括号

--- src/dp/LongestBracket.java
+++ src/dp/LongestBracket.java
@@ -1,12 +1,31 @@
 package dp;
 
+import java.util.Arrays;
+import java.util.Stack;
+
 public class LongestBracket {
-//
-//    public int longestValidParentheses(String s) {
-//
-//    }
+
+
+    public int longestValidParentheses(String s) {
+        int maxans = 0;
+        int dp[] = new int[s.length()];
+        for (int i = 1; i < s.length(); i++) {
+            if (s.charAt(i) == ')') {
+                if (s.charAt(i - 1) == '(') {
+                    dp[i] = (i >= 2 ? dp[i - 2] : 0) + 2;
+                } else if (i - dp[i - 1] > 0 && s.charAt(i - dp[i - 1] - 1) == '(') {
+                    dp[i] = dp[i - 1] + ((i - dp[i - 1]) >= 2 ? dp[i - dp[i - 1] - 2] : 0) + 2;
+                }
+                maxans = Math.max(maxans, dp[i]);
+            }
+        }
+        return maxans;
+    }
+
 
     public static void main(String[] args) {
-
+        String str = "()(())";
+        LongestBracket longestBracket = new LongestBracket();
+        System.out.println(longestBracket.longestValidParentheses(str));
     }
 }
