3ba23269862ee1d06a88d38a334f8b166964e426
Fatead <893051689@qq.com>
2020-06-13 18:36:08 +0800

    打卡48，旋转图像

---
+++ src/array/RotateImage.java
@@ -0,0 +1,38 @@
+package array;
+
+import java.util.Arrays;
+
+public class RotateImage {
+
+    public void rotate(int[][] matrix) {
+        int n = matrix[0].length;
+        if(n == 0)return;
+        //先做矩阵的转置
+        for(int i = 0;i<n;i++){
+            for(int j =0;j<i;j++){
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+        for(int i =0;i<n;i++){
+            for(int j = 0;j<n/2;j++){
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[i][n-j-1];
+                matrix[i][n-j-1] = temp;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[][] matrix = {
+                {1,2,3},
+                {4,5,6},
+                {7,8,9}
+        };
+        RotateImage rotateImage = new RotateImage();
+        rotateImage.rotate(matrix);
+        System.out.println(Arrays.deepToString(matrix));
+
+    }
+}
